34890a6c8d7a8fbcf019d6343327bf60bf683e1d
yuki-tea <68893628+yuki-tea@users.noreply.github.com>
2020-10-04 16:42:26 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+lock "3.14.1"
 
 set :application, "fleamarket_sample_80c"
 set :repo_url, "git@github.com:yuki-tea/fleamarket_sample_80c.git"
