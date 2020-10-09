427ac5afe304f8da5606b69fc6c831c40edb18b5
rikuto1020 <67956343+rikuto1020@users.noreply.github.com>
2020-10-09 20:40:54 +0900

    出品ページのhaml書きました

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'messages/index'
+
+  get 'listings/new'
   root "messages#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
