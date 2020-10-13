83e35f16d7a4285a3c8bc6ad6e1e3f239711263e
rikuto1020 <67956343+rikuto1020@users.noreply.github.com>
2020-10-13 20:23:47 +0900

    item,imageモデル作成しました。

---
+++ db/migrate/20201013104708_create_items.rb
@@ -0,0 +1,19 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      #t.string :name, null: false
+      #t.index :name, unique: true
+      #t.references :user, foreign_key: true
+      t.integer :user_id, null: false ,foreign_key: true
+      t.string :name, null: false 
+      t.text :description, null: false
+      t.integer :category_id , null:false,foreign_key: true
+      t.text :brand
+      t.integer :item_status_id, null: false
+      t.integer :shipping_charge_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :price, null: false
+      t.timestamps
+    end
+  end
+end
