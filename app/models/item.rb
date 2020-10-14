e9c1eee4d295d988a2618fc47fee8933725b6c81
rikuto1020 <67956343+rikuto1020@users.noreply.github.com>
2020-10-14 20:45:22 +0900

    itemモデル編集しました。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :item_status
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :handling_time
 end
