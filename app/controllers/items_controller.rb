822123b8b153ba6df6e3a5e47affb27dbbf9868c
rikuto1020 <67956343+rikuto1020@users.noreply.github.com>
2020-10-14 20:01:10 +0900

    itemsコントローラーファイル記述しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
-  def index
+
+  def new
+     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if item.save
+      redirect_to root_path, notice: "出品が完了しました"
+    else
+      render.new
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(user_ids: [] , :name. :description, categories_id: [], :brand, item_statuses_id: [], shipping_charges_id: [],prefectures_id: [], :price)
+  end
+
 end
