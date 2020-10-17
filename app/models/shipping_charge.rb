2c79c880152bc1e111f9a280d8e62a30ef6ab77d
rikuto1020 <67956343+rikuto1020@users.noreply.github.com>
2020-10-17 16:00:16 +0900

    商品状態選択できるようになりました。

--- app/models/shipping_charge.rb
+++ app/models/shipping_charge.rb
@@ -1,3 +1,4 @@
-class Shipping_charge < ActiveHash::Base
+class ShippingCharge < ActiveHash::Base
   self.data = [
-    {id: 1, name: '送料込み（出品者負担）'}, {id: , name: '送料込み（購入者負担）'} ]
+    {id: 1, name: '送料込み（出品者負担）'}, {id:2 , name: '送料込み（購入者負担）'} ]
+end
