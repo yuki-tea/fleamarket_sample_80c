c2a5ce837e8415f2efe5be8f08dbd07ec5566f26
rikuto1020 <67956343+rikuto1020@users.noreply.github.com>
2020-10-17 12:58:23 +0900

    mugrationファイル編集しました

--- db/migrate/20201007132639_devise_create_users.rb
+++ db/migrate/20201007132639_devise_create_users.rb
@@ -5,8 +5,25 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :name,               null: false
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, unique: true, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :first_name_kana,    null: false
+      t.string :last_name_kana,     null: false
+      t.string :birthday,           null: false
+      t.string :delivery_first_name, null: false
+      t.string :delivery_last_name, null: false
+      t.string :delivery_first_name_kana, null: false
+      t.string :delivery_last_name_kana, null: false
+      t.integer :prefecture_id, null: false
+      t.string :zip_code, null: false
+      t.string :municipality, null: false
+      t.string :street_number, null: false
+      t.string :building_name
+      t.string :telephone_number, null: false, unique: true
+
+
 
       ## Recoverable
       t.string   :reset_password_token
@@ -37,9 +54,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
     
-    add_index :users, :name,                 unique: true
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+
+     add_index :users, :email,                 unique: true
+
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
