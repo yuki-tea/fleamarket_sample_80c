84629c114e8dfbde10d233e326f7a59e880ad17d
yuki-tea <68893628+yuki-tea@users.noreply.github.com>
2020-10-17 14:22:34 +0900

    10/17.3

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :messages, only: :index
   resources :listings, only: :new
   root "messages#index"
+  get "users/show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
