d04ced53e2084c162af266a17c28f1e7e192b057
TaigaMito <69384486+TaigaMito@users.noreply.github.com>
2020-10-18 20:25:39 +0900

    10/18

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   devise_for :users
   resources :messages, only: :index
   resources :listings, only: :new
+=======
+  get 'buyers/index'
+  get 'messages/index'
+>>>>>>> Stashed changes
   root "messages#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
