695af825d18778a88324c260e24f46bf35383c53
TaigaMito <69384486+TaigaMito@users.noreply.github.com>
2020-10-18 21:03:42 +0900

    10/18

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw doUpdated upstream
   resources :messages, only: :index
   resources :listings, only: :new
   get 'buyers/index'
-  get 'messages/index'
-  root "messages#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
