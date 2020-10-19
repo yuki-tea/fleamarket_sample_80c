ab22dfa988220623e554334646e5a4c942412bbc
yuki-tea <68893628+yuki-tea@users.noreply.github.com>
2020-10-19 21:27:31 +0900

    10/19.1

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,40 +1,43 @@
 class BuyersController < ApplicationController
-  require 'payjp'#Payjpの読み込み
-  before_action :set_card, :set_item
+  # require 'payjp'#Payjpの読み込み
+  # before_action :set_card, :set_item
 
   def index
-    if @card.blank?
-      #登録された情報がない場合にカード登録画面に移動
-      redirect_to new_card_path
-    else
-      Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
-      #保管した顧客IDでpayjpから情報取得
-      customer = Payjp::Customer.retrieve(@card.customer_id) 
-      #カード情報表示のためインスタンス変数に代入
-      @default_card_information = customer.cards.retrieve(@card.card_id)
-    end
+    # if @card.blank?
+    #   #登録された情報がない場合にカード登録画面に移動
+    #   redirect_to new_card_path
+    # else
+    #   Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
+    #   #保管した顧客IDでpayjpから情報取得
+    #   customer = Payjp::Customer.retrieve(@card.customer_id) 
+    #   #カード情報表示のためインスタンス変数に代入
+    #   @default_card_information = customer.cards.retrieve(@card.card_id)
+    # end
   end
 
   def pay
-    Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
-    Payjp::Charge.create(
-      :amount => @item.price, #支払金額を引っ張ってくる
-      :customer => @card.customer_id,  #顧客ID
-      :currency => 'jpy',              #日本円
-    )
+    # Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
+    # Payjp::Charge.create(
+    #   :amount => @item.price, #支払金額を引っ張ってくる
+    #   :customer => @card.customer_id,  #顧客ID
+    #   :currency => 'jpy',              #日本円
+    # )
+
+  end
+
+  def show
 
   end
 
 
+  # private
 
-  private
+  # def set_card
+  #   @card = Card.find_by(user_id: current_user.id)
+  # end
 
-  def set_card
-    @card = Card.find_by(user_id: current_user.id)
-  end
-
-  def set_item
-    @item = Item.find(params[:item_id])
-  end
+  # def set_item
+  #   @item = Item.find(params[:item_id])
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
-Rails.application.routes.draw doUpdated upstream
-  devise_for :users
+Rails.application.routes.draw do
   resources :messages, only: :index
   resources :listings, only: :new
-  get 'buyers/index'
-
+  root "messages#index"
+  resources :buyers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
