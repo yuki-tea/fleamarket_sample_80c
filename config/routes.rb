bd0071d924c945b53e1fa2aad0a86b253fcf0442
TaigaMito <69384486+TaigaMito@users.noreply.github.com>
2020-10-19 23:24:29 +0900

    10.19

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :messages, only: :index
   resources :listings, only: :new
   root "messages#index"
-  resources :buyers
+  resources :buyers, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
