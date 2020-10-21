91d7642471cd36370609803059d150ddd4d14f5b
chunyurina <yurina8ru@gmail.com>
2020-10-21 19:55:19 +0900

    ログアウト画面とプロフィール画面とクレジット登録画面

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :listings, only: :new
   root "messages#index"
   get "users/show"
+  get "users/logout"
+  get "cards/show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
