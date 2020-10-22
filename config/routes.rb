431ebbd42be9e0eddf6481cf65e7ed697f090d5a
naoya0240 <yoshioka0240@gmail.com>
2020-10-22 21:00:48 +0900

    routes.rb変更

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,4 @@ Rails.application.routes.draw do
   resources :cards, only: :show
   resources :items, only: :show
   root "messages#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
