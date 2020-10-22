0f222e5dd913fb3ad50501fbbf39ea654b4b3c6e
chunyurina <yurina8ru@gmail.com>
2020-10-22 19:23:45 +0900

    ビューの大枠

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :listings, only: :new
   resources :users, only: [:show, :destroy]
   resources :cards, only: :show
+  resources :items, only: :show
   root "messages#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
