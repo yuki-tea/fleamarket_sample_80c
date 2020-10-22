9a02f4f6de62d7dff8eedb8c08f66d1a7b358712
yuki-tea <68893628+yuki-tea@users.noreply.github.com>
2020-10-22 20:28:36 +0900

    10/22.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :messages, only: :index
   resources :listings, only: :new
-  resources :buyers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  resources :users, only: [:show, :destroy]
+  resources :cards, only: :show
+  resources :items, only: :show
+  root "messages#index"
