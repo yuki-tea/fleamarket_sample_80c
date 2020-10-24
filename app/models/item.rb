42378838c0e53ad77b8fe6f9d6c8e1dc145343a6
Daisuke073 <cpxyh073@gmail.com>
2020-10-24 13:26:00 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,14 @@ class Item < ApplicationRecord
   has_many :images
 
   accepts_nested_attributes_for :images, allow_destroy: true
+  
+  validates :user_id, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :item_status_id, presence: true
+  validates :shipping_charge_id
+  validates :prefecture_id
+  validates :handling_time_id
+  validates :price
 end
