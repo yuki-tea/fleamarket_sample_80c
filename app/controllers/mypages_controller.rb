a45e37381527c67854877784e2a5be8bb8efc5fd
TaigaMito <69384486+TaigaMito@users.noreply.github.com>
2020-10-24 13:58:53 +0900

    10/24
    mypagesコントローラ作成
    →マイページ画面のビューが見れなかったため

---
+++ app/controllers/mypages_controller.rb
@@ -0,0 +1,7 @@
+class MypagesController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :set_address, only: [:edit, :update]
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
