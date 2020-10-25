d17d16dd714a8299ddcf110d3146539036583487
rikuto1020 <67956343+rikuto1020@users.noreply.github.com>
2020-10-25 20:52:20 +0900

    10/25

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,14 +7,15 @@ class ItemsController < ApplicationController
   def new
      @item = Item.new
      @item.images.new
+     @item.users << current_user
   end
 
   def create
     @item = Item.new(item_params)
-    if item.save
+    if @item.save
       redirect_to root_path, notice: "出品が完了しました"
     else
-      render.new
+      render :new
       flash.now[:alert] = "商品出品に失敗しました"
   end
 
@@ -26,12 +27,10 @@ class ItemsController < ApplicationController
 
   def destroy
   end
-
-
+end
   private
 
   def item_params
-    params.require(:item).permit( :name, :description, :categories_id, :brand, :item_statuses_id, :shipping_charges_id,:prefectures_id , :price, images_attributes: [:image].merge(user_id: current_user.id))
-  end
-end
+    params.require(:item).permit( :name, :description, :category_id, :brand, :item_status_id, :shipping_charge_id,:prefecture_id , :handling_time_id,:price, images_attributes: [:image].merge(user_id: current_user.id))
+ end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+lady = Category.create(:name=>"レディース")
+
+lady_tops = lady.children.create(:name=>"トップス")
+lady_jacket = lady.children.create(:name=>"ジャケット/アウター")
+
+lady_tops.children.create([{:name=>"Tシャツ/カットソー(半袖/袖なし)"}, {:name=>"Tシャツ/カットソー(七分/長袖)"},{:name=>"その他"}])
+lady_jacket.children.create([{:name=>"テーラードジャケット"}, {:name=>"ノーカラージャケット"}, {:name=>"Gジャン/デニムジャケット"},{:name=>"その他"}])
+
+
+@category1 = Category.create(name:"カテゴリ1")
+categories = [
+             {level1:"カテゴリ 1",level_children[
+             {level2: "カテゴリ 1-1", level2_children:["1-1-1","1-1-2","1-1-3"]}]
+             },
+             {level1:"カテゴリ 2",level1_children:[
+              {level2:"カテゴリ 2-1",level2_children:["カテゴリ 2-1-1","カテゴリ 2-1-2","カテゴリ 2-1-3"]},
+              {level2:"カテゴリ 2-2",level2_children:["カテゴリ 2-2-1","カテゴリ 2-2-2"]}
+             ]
+}
+]
+# each.with_indexの使い方
+# 使うと要素の数だけ処理を繰り返し実行し、繰り返しごとに番号を振ることができます。
+# iに番号が入る。
+
+
+
+
+
+categories.each.with_index(1) do |category1,i|
+  # カテゴリーにインデックスを貼る
+  level1_var = "@category1_#{i}"
+  level1_val = Category.create(name: "#{category[level1]}")eval("#{level1_val} = level1_val")
+    category[:level1_children].each.with_index(1) do |level1_child, j|
+  # evalとは、引数で渡した文字列rubyプログラムとして実行するメソッド
+    level2_var ="#{level1_var}_#{j}"
+    level2_val= eval("#{level1_var}.children.create(name:level1_child[:level2])")eval("#{level2_var} = level2_val")
+      level1_child[:level2_children].each do |level2_children_val|
+        eval("#{level2_var}.children.create(name:level2_children_val)") 
+      end
+  end
+end
