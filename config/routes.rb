c4e3938fc89f62efe7778393c8dfb8ac314fd202
naoya0240 <yoshioka0240@gmail.com>
2020-10-29 22:03:08 +0900

    再度変更しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :messages, only: :index
-  resources :listings, only: :new
+  resources :items, only: [:show, :new]
   resources :cards, only: :show
   resources :buyers, only: :show
   resources :mypages, only: [:show, :destroy, :new]
-  resources :item_details, only: :show
   root "messages#index"
 end
