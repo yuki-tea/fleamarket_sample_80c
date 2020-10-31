b62370097e4805812c99374ba8aab5b28f38a398
Daisuke073 <cpxyh073@gmail.com>
2020-10-31 17:54:09 +0900

    出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,24 +22,25 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @category_parent_array = Category.where(ancestry: nil) 
+    # @category_parent_array = Category.where(ancestry: nil) 
+    # @category_children = Category.find(@category_parent_array).children
+    # @category_grandchildren = Category.find(@category_children).children
+    
+    # binding.pry
     
     if @item.save
-      saling_items = SalingItems.new(item_id: @item.id, saler_id: params[:user_id], status: "出品中")
-      saler = Saler.new(item_id: @item.id, user_id: params[:user_id])
-      
-      binding.pry
-      
-      if selling_status.save && seller.save
+      # saling_items = SalingItems.new(item_id: @item.id, saler_id: params[:user_id], status: "出品中")
+      # saler = Saler.new(item_id: @item.id, user_id: params[:user_id])
+            
         redirect_to root_path, notice: "出品が完了しました"
       else
         render :new
         flash.now[:alert] = "商品出品に失敗しました"
       end
-    else
-      flash.now[:alert] = '入力されていない項目があります。'
-      render :new
-    end
+    # else
+    #   flash.now[:alert] = '入力されていない項目があります。'
+    #   render :new
+    # end
   end
 
   def edit
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,8 @@ class Item < ApplicationRecord
 
   belongs_to :category
   belongs_to :user
-  belongs_to :saler, class_name: "User"
-  belongs_to :buyer, class_name: "User"
+  # belongs_to :saler, class_name: "User"
+  # belongs_to :buyer, class_name: "User"
 
   has_many :images
 
