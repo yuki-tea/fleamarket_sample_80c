41d0949b6a1769fa61aff813c534bfee094f277a
Daisuke073 <cpxyh073@gmail.com>
2020-11-01 15:32:51 +0900

    コンフリ解消２

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :messages, only: :index
-  resources :items, only: [:show, :new]
   resources :cards, only: :show
   resources :buyers, only: :show
   resources :mypages, only: [:show, :destroy, :new]
