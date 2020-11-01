e562fac8244b2dfa096d639960d25d835347a79c
naoya0240 <yoshioka0240@gmail.com>
2020-11-01 16:20:52 +0900

    item追記しました

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :messages, only: :index
   resources :cards, only: :show
   resources :buyers, only: :show
+  resources :items, only: [:show, :new]
   resources :mypages, only: [:show, :destroy, :new]
   root "messages#index"
 end
