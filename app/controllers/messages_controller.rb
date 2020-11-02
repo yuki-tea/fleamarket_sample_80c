1b47f5baffb8842e42546c3acd06fb1ead755c62
chunyurina <yurina8ru@gmail.com>
2020-11-02 20:30:03 +0900

    second

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,7 @@
 class MessagesController < ApplicationController
   def index
-    @image = Image.includes(:image).order('created_at DESC')
+    # @image = Image.includes(:image).order('created_at DESC')
+    # @item = Item.includes(:images).order('created_at DESC')
+    @item = Item.find(params[:id])
   end
 end
