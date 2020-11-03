d7b50dfa7f3e690fd3fc8afbf5a590303dc303c9
Daisuke073 <cpxyh073@gmail.com>
2020-11-03 19:46:17 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,8 +50,4 @@ class ItemsController < ApplicationController
     params.require(:item).permit( :name, :description, :category_id, :brand, :item_status_id, :shipping_charge_id,:prefecture_id , :handling_time_id,:price, images_attributes: [:image]).merge(user_id: current_user.id)
   end
 
-  # def move_to_index
-  #   redirect_to action: :index unless user_signed_in?
-  # end
-
 end
