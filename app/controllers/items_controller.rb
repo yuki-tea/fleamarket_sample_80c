29ca64648bfcc477c1d4209262643700a9807ecc
chunyurina <yurina8ru@gmail.com>
2020-11-04 20:33:43 +0900

    指摘箇所修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  # before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
   
   # def index
   #   @item = Item.includes(:images).order('created_at DESC')
@@ -58,8 +58,8 @@ class ItemsController < ApplicationController
     params.require(:item).permit( :name, :description, :category_id, :brand, :item_status_id, :shipping_charge_id,:prefecture_id , :handling_time_id,:price, images_attributes: [:image]).merge(user_id: current_user.id)
   end
 
-  # def move_to_index
-  #   redirect_to action: :index unless user_signed_in?
-  # end
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 
 end
