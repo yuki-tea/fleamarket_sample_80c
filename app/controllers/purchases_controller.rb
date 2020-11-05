993db17279822d96a4960084446caf4fe7d16484
chunyurina <yurina8ru@gmail.com>
2020-11-05 21:23:44 +0900

    ルーティングの設定と購入画面へのパスを通しました

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,30 +1,34 @@
 class PurchasesController < ApplicationController
 
-  require 'payjp'
-
   def index
-    binding.pry
-    @item = Item.new
-    card = Card.where(user_id: current_user.id).first
-    # 登録された情報がない場合、登録画面に移動
-    if card.blank?
-      redirect_to controller: "cards", action: "new"
-    else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
-    end
+    
   end
 
-  def pay
-    @item = Item.find(params[:id])
-    card = Card.where(user_id: current_user.id).first
-    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    Payjp::Charge.create(
-      :amount => @item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
-      :customer => card.customer_id, #顧客ID
-      :currency => 'jpy', #日本円
-    )
-    redirect_to action: 'done' #完了画面に移動
-    end
+  # require 'payjp'
+
+  # def index
+  #   # binding.pry
+  #   # @item = Item.new
+  #   card = Card.where(user_id: current_user.id).first
+  #   # 登録された情報がない場合、登録画面に移動
+  #   if card.blank?
+  #     redirect_to controller: "cards", action: "new"
+  #   else
+  #     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+  #     customer = Payjp::Customer.retrieve(card.customer_id)
+  #     @default_card_information = customer.cards.retrieve(card.card_id)
+  #   end
+  # end
+
+  # def pay
+  #   @item = Item.find(params[:id])
+  #   card = Card.where(user_id: current_user.id).first
+  #   Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+  #   Payjp::Charge.create(
+  #     :amount => @item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
+  #     :customer => card.customer_id, #顧客ID
+  #     :currency => 'jpy', #日本円
+  #   )
+  #   redirect_to action: 'done' #完了画面に移動
+  #   end
   end
