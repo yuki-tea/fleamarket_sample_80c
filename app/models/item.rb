98429c63d627031ee18c14f64d56238d91fe3cba
rikuto1020 <67956343+rikuto1020@users.noreply.github.com>
2020-11-07 11:32:04 +0900

    model編集しました

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,6 @@ class Item < ApplicationRecord
 
   belongs_to :category
   belongs_to :user
-  # belongs_to :purchase
   # belongs_to :saler, class_name: "User"
   # belongs_to :buyer, class_name: "User"
 
