f56d572bd9a78eba641c34f09b92c43b0e7ed394
yuki-tea <68893628+yuki-tea@users.noreply.github.com>
2020-11-20 21:25:50 +0900

    画像保存

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_fit: [100, 100] 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
